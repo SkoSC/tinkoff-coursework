7cc2c97c54b13938dde71a32f1995f2ed50da1f2
Alexey <scorpius98.alex@gmail.com>
2018-10-31 23:00:09 +0300

    Refactoring

--- app/src/main/java/com/skosc/tkffintech/ui/activity/MainActivity.kt
+++ app/src/main/java/com/skosc/tkffintech/ui/activity/MainActivity.kt
@@ -8,30 +8,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 import com.skosc.tkffintech.R
 
 class MainActivity : AppCompatActivity() {
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+
+        }
+        false
     }
 }
