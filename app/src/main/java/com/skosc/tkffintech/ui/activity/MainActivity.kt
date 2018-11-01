2cb2a7f230409781eec31b6dfa26f145c955022e
Alexey <scorpius98.alex@gmail.com>
2018-11-01 09:14:05 +0300

    Auto integration of menu and navController

--- app/src/main/java/com/skosc/tkffintech/ui/activity/MainActivity.kt
+++ app/src/main/java/com/skosc/tkffintech/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 import com.skosc.tkffintech.R
 
@@ -15,15 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_course -> navController.navigate(R.id.navg_coursesFragment)
-            R.id.navigation_event -> navController.navigate(R.id.navg_eventsFragment)
-            R.id.navigation_profile -> navController.navigate(R.id.navg_profileFragment)
-        }
-        true
+        NavigationUI.setupWithNavController(navigation, navController)
     }
 }
