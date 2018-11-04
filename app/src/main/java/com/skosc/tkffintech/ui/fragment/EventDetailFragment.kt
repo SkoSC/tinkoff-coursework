35af8374d6817f0b18e87ca7105dfaaf31ea5034
Alexey <scorpius98.alex@gmail.com>
2018-11-05 02:31:28 +0300

    Event detail fragment styled

--- app/src/main/java/com/skosc/tkffintech/ui/fragment/EventDetailFragment.kt
+++ app/src/main/java/com/skosc/tkffintech/ui/fragment/EventDetailFragment.kt
@@ -6,9 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.get
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
 
 import com.skosc.tkffintech.R
 import com.skosc.tkffintech.ui.model.EventCardModel
+import kotlinx.android.synthetic.main.fragment_event_detail.*
+import com.google.android.gms.maps.model.MarkerOptions
+import android.location.Geocoder
+import java.util.*
+
 
 private const val ARG_MODEL = "model"
 
@@ -29,13 +37,20 @@ class EventDetailFragment : Fragment() {
     }
 
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                EventDetailFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_MODEL, param2)
-                    }
-                }
+    override fun onStart() {
+        super.onStart()
+        event_detail_map.onCreate(null)
+        event_detail_map.getMapAsync {
+            it.uiSettings.setAllGesturesEnabled(false)
+            val geoCoder = Geocoder(context, Locale.getDefault())
+            val address = geoCoder.getFromLocationName("Москва", 1).first()
+            val seattle = LatLng(address.latitude, address.longitude)
+            it.addMarker(MarkerOptions().position(seattle).title("Москва"))
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(seattle, 12F))
+
+            event_detail_map.onResume()
+        }
     }
+
+
 }
