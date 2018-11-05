d13d70242f6e6ac0a26758233b3ed6d5d919fbd0
Alexey <scorpius98.alex@gmail.com>
2018-11-06 02:04:06 +0300

    Fixed nullability

--- app/src/main/java/com/skosc/tkffintech/utils/DateTimeFormatter.kt
+++ app/src/main/java/com/skosc/tkffintech/utils/DateTimeFormatter.kt
@@ -5,5 +5,5 @@ import org.joda.time.format.DateTimeFormatter
 
 object DateTimeFormatter {
     val DATE_TIME_FORMATTER_FULL: DateTimeFormatter get() = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
-    val DATE_FORMATTER_SHORT_EU get() = DateTimeFormat.forPattern("dd-MM-yyyy")
+    val DATE_FORMATTER_SHORT_EU: DateTimeFormatter get() = DateTimeFormat.forPattern("dd-MM-yyyy")
 }
