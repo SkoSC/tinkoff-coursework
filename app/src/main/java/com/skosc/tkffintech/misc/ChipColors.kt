301c9e021073f2e280fd86ff5f865ed6e0300594
Alexey <scorpius98.alex@gmail.com>
2018-11-06 11:02:18 +0300

    Added colors search module

---
+++ app/src/main/java/com/skosc/tkffintech/misc/ChipColors.kt
@@ -0,0 +1,31 @@
+package com.skosc.tkffintech.misc
+
+import androidx.annotation.ColorRes
+import com.skosc.tkffintech.R
+import java.util.*
+
+object ChipColors {
+    private val allAllowedColors = arrayOf(
+            R.color.event_chip_blue,
+            R.color.event_chip_green,
+            R.color.event_chip_red,
+            R.color.event_chip_purple
+    )
+
+    @ColorRes
+    fun randomColor(): Int {
+        val rnd = Random()
+        val randomColorId = rnd.nextInt(3)
+        return allAllowedColors[randomColorId]
+    }
+
+    @ColorRes
+    fun colorForName(name: String): Int {
+        return when(name.trim().toLowerCase()) {
+            "purple" -> R.color.event_chip_purple
+            "green" -> R.color.event_chip_green
+            "orange" -> R.color.event_chip_red
+            else -> randomColor()
+        }
+    }
+}
