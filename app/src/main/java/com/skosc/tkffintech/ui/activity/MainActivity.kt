27b77b7750006fe45528bc1d1b6f7b25b2484c3f
Alexey <scorpius98.alex@gmail.com>
2018-11-06 18:58:11 +0300

    Added search option icon

--- app/src/main/java/com/skosc/tkffintech/ui/activity/MainActivity.kt
+++ app/src/main/java/com/skosc/tkffintech/ui/activity/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.skosc.tkffintech.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.Observer
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.skosc.tkffintech.R
@@ -9,8 +13,13 @@ import com.skosc.tkffintech.viewmodel.events.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : TKFActivity() {
+    companion object {
+        private const val UNKNOWN_DESTINATION = 0
+    }
+
     val navController by lazy { findNavController(nav_host_fragment) }
     val vm by lazy { getViewModel(MainActivityViewModel::class) }
+    private var searchMenuItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,21 +28,40 @@ class MainActivity : TKFActivity() {
         NavigationUI.setupWithNavController(navigation, navController)
 
         vm.isLoggedIn.observe(this, Observer {
-            // TODO Add actual check
-            //if (!it) startActivity(Intent(this, LoginActivity::class.java))
+            if (!it) startActivity(Intent(this, LoginActivity::class.java))
         })
 
         navController.addOnNavigatedListener { _, destination ->
-            when (destination.id) {
-                R.id.navigation_profile -> supportActionBar?.hide()
-                R.id.navigation_event_detail -> supportActionBar?.hide()
-                else -> supportActionBar?.show()
-            }
+            updateSearchBarVisibility(destination)
+            updateAppBarVisibility(destination)
         }
-
     }
 
     override fun onBackPressed() {
         navController.navigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        searchMenuItem = menu?.getItem(0)
+        updateSearchBarVisibility()
+        return true
+    }
+
+    private fun updateSearchBarVisibility(destination: NavDestination? = navController.currentDestination) {
+        searchMenuItem?.let { item ->
+            item.isVisible = when (destination?.id ?: UNKNOWN_DESTINATION) {
+                R.id.navigation_event_list -> true
+                else -> false
+            }
+        }
+    }
+
+    private fun updateAppBarVisibility(destination: NavDestination? = navController.currentDestination) {
+        when (destination?.id ?: UNKNOWN_DESTINATION) {
+            R.id.navigation_profile -> supportActionBar?.hide()
+            R.id.navigation_event_detail -> supportActionBar?.hide()
+            else -> supportActionBar?.show()
+        }
     }
 }
