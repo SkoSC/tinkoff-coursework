04b154fee4d9e1e7d5b1716c266075719c335fe0
Alexey <scorpius98.alex@gmail.com>
2018-11-23 01:31:43 +0300

    Changed http logging format

--- app/src/main/java/com/skosc/tkffintech/utils/OkHttpLoggingInterceptor.kt
+++ app/src/main/java/com/skosc/tkffintech/utils/OkHttpLoggingInterceptor.kt
@@ -3,22 +3,30 @@ package com.skosc.tkffintech.utils
 import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.util.concurrent.atomic.AtomicLong
 
 /**
  * Logs intercepted requests of OKHTTP
  */
 class OkHttpLoggingInterceptor(private val tag: String) : Interceptor {
+    private val requestCounter = AtomicLong(0)
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
 
+        val id = requestCounter.getAndIncrement()
 
-        Log.d(tag, "Request: " + request.url() + "\nHeaders:\n" + request.headers())
+        log("Request $id: (${request.method()}) ${request.url()}")
 
         val response = chain.proceed(request)
 
-        Log.d(tag, "Response: " + response.message() + "\nHeaders:\n" + response.headers())
+        log("Response $id: ${response.code()}/${response.message()}")
 
         return response
+    }
+
+    private fun log(msg: String) {
+        Log.d(tag, msg)
     }
 
 }
