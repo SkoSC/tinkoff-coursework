e7b13219e0613cebae529699cc94df09f57c49e6
Alexey <scorpius98.alex@gmail.com>
2018-11-23 01:44:07 +0300

    WIP

--- app/src/main/java/com/skosc/tkffintech/model/repo/HomeworkRepo.kt
+++ app/src/main/java/com/skosc/tkffintech/model/repo/HomeworkRepo.kt
@@ -3,10 +3,13 @@ package com.skosc.tkffintech.model.repo
 import com.skosc.tkffintech.entities.Homework
 import com.skosc.tkffintech.entities.HomeworkGrade
 import com.skosc.tkffintech.entities.TaskWithGrade
+import com.skosc.tkffintech.misc.DataUpdateResult
 import com.skosc.tkffintech.viewmodel.UserWithGradesSum
 import io.reactivex.Observable
+import io.reactivex.Single
 
 
 interface HomeworkRepo {
-
+    fun checkForUpdates(course: String): Single<DataUpdateResult>
+    fun tryForceRefresh(course: String): Single<DataUpdateResult>
 }
--- app/src/main/java/com/skosc/tkffintech/model/repo/HomeworkRepoImpl.kt
+++ app/src/main/java/com/skosc/tkffintech/model/repo/HomeworkRepoImpl.kt
@@ -1,24 +1,18 @@
 package com.skosc.tkffintech.model.repo
 
 import android.content.SharedPreferences
-import com.skosc.tkffintech.entities.Homework
-import com.skosc.tkffintech.entities.HomeworkGrade
-import com.skosc.tkffintech.entities.TaskWithGrade
-import com.skosc.tkffintech.entities.User
 import com.skosc.tkffintech.misc.DataUpdateResult
 import com.skosc.tkffintech.model.entity.ExpirationTimer
-import com.skosc.tkffintech.model.room.*
-import com.skosc.tkffintech.model.room.model.*
+import com.skosc.tkffintech.model.room.CourseInfoDao
+import com.skosc.tkffintech.model.room.GradesDao
+import com.skosc.tkffintech.model.room.HomeworkDao
+import com.skosc.tkffintech.model.room.UserDao
 import com.skosc.tkffintech.model.service.NetworkInfoService
 import com.skosc.tkffintech.model.webservice.TinkoffCursesApi
 import com.skosc.tkffintech.model.webservice.TinkoffGradesApi
-import com.skosc.tkffintech.utils.mapEach
-import com.skosc.tkffintech.utils.own
-import com.skosc.tkffintech.viewmodel.UserWithGradesSum
-import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
-import org.joda.time.DateTime
+import retrofit2.Response
 
 class HomeworkRepoImpl(
         private val gradesApi: TinkoffGradesApi,
@@ -27,7 +21,7 @@ class HomeworkRepoImpl(
         private val gradesDao: GradesDao,
         private val userDao: UserDao,
         private val homeworkDao: HomeworkDao,
-        timersSharedPreferences: SharedPreferences,
+        private val timersSharedPreferences: SharedPreferences,
         private val networkInfo: NetworkInfoService
 ) : HomeworkRepo {
     companion object {
@@ -35,7 +29,44 @@ class HomeworkRepoImpl(
     }
 
     private val cdisp = CompositeDisposable()
-    private val expTimer = ExpirationTimer.create(timersSharedPreferences, "homework-update")
+    private val dataRefresh = HashMap<String, ExpirationTimer>()
 
 
+    private val dataFreshUtil = ExpirationTimer.create(timersSharedPreferences, "homework-update")
+
+    override fun tryForceRefresh(course: String): Single<DataUpdateResult> {
+        TODO()
+    }
+
+    override fun checkForUpdates(course: String): Single<DataUpdateResult> {
+        return needsUpdate(course).flatMap {
+            if (it) {
+                tryForceRefresh(course)
+            } else {
+                Single.just(DataUpdateResult.NotUpdated)
+            }
+        }
+    }
+
+    private fun needsUpdate(course: String): Single<Boolean> {
+        if (course in dataRefresh) {
+            return dataRefresh[course]!!.isExpired
+        }
+
+        val key = makeRefreshKey(course)
+        dataRefresh[course] = ExpirationTimer.create(timersSharedPreferences, key)
+        return Single.just(true)
+    }
+
+    private fun makeRefreshKey(course: String): String {
+        return "homework-refresh-$course"
+    }
+
+    private fun successToUpdateResult(resp: Response<*>): DataUpdateResult {
+        return if (resp.isSuccessful) {
+            DataUpdateResult.Updated
+        } else {
+            DataUpdateResult.Error
+        }
+    }
 }
