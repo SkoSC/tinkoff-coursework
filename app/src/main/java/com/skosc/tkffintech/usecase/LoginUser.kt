a63c734bdb35a08f899619ba009581db0a42ebf7
Alexey <scorpius98.alex@gmail.com>
2018-11-29 00:13:05 +0300

    LoginUser now performs preload

--- app/src/main/java/com/skosc/tkffintech/usecase/LoginUser.kt
+++ app/src/main/java/com/skosc/tkffintech/usecase/LoginUser.kt
@@ -1,16 +1,35 @@
 package com.skosc.tkffintech.usecase
 
+import com.skosc.tkffintech.misc.UpdateResult
 import com.skosc.tkffintech.model.repo.CurrentUserRepo
 import com.skosc.tkffintech.utils.extensions.mapEach
 import com.skosc.tkffintech.utils.logging.LoggerProvider
 import io.reactivex.Single
+import java.util.concurrent.TimeUnit
 
+/**
+ * Performs user login. Updates data if possible
+ */
 class LoginUser(private val currentUserRepo: CurrentUserRepo, private val loadCourses: LoadCourses, private val loadHomeworks: LoadHomeworks) {
+    companion object {
+        /**
+         * Arbitrary delay between loading courses and updating homeworks info
+         */
+        private const val UPDATE_DELAY = 3L
+    }
+
     private val logger = LoggerProvider.get(this)
 
+    /**
+     * Leggins user to server.
+     *
+     * @return Success value of login
+     */
     fun login(email: String, password: String): Single<Boolean> {
+        logger.info("Trying to login...")
         return currentUserRepo.login(email, password)
                 .doOnSuccess { success ->
+                    logger.info("Login is successful: $success")
                     if (success) {
                         tryPreload()
                     }
@@ -21,10 +40,22 @@ class LoginUser(private val currentUserRepo: CurrentUserRepo, private val loadCo
      * Tries to preload data for user, fails silently
      */
     private fun tryPreload() {
-        val disp = loadCourses.checkForUpdates().flatMap {
-            loadCourses.courses.mapEach { loadHomeworks.checkForUpdates(it.url).blockingGet() }
-        }.subscribe({}, {
-            logger.warning("Preload failed, reason: ${it.message}")
-        })
+        logger.verbose("Trying to preload data")
+        val disp = loadCourses.checkForUpdates()
+                .delay(UPDATE_DELAY, TimeUnit.SECONDS) //TODO Dirty hack, remove
+                .flatMap {
+                    blockingUpdateCourses()
+                }.subscribe({
+                    val preloaded = it.filter { it == UpdateResult.Updated }.size
+                    val total = it.size
+                    logger.verbose("Persuading success: $preloaded/$total")
+                }, {
+                    logger.warning("Preload failed, reason: ${it.message}")
+                })
     }
+
+    private fun blockingUpdateCourses() = loadCourses.courses
+            .mapEach { course ->
+                loadHomeworks.checkForUpdates(course.url).blockingGet()
+            }
 }
